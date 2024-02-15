52de612d039928e1e17d3f0c596f9e1adb09ea6e
Mahtabvariyani <mahtabvariyani@gmail.com>
2024-02-15 12:26:52 +0100

    Fixed

--- components/Second.jsx
+++ components/Second.jsx
@@ -8,7 +8,7 @@ const Example = () => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsTextVisible(true); // Set the state to make the text visible after 1 second
-    }, 3000);
+    }, 3500);
 
     return () => clearTimeout(timeout); // Cleanup function
   }, []); // Empty dependency array ensures it runs only once
